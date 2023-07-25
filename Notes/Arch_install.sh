14a9494f8bd1303c1f9dfb8bf157df318e92e28f
draihanx <114208911+draihanx@users.noreply.github.com>
2023-07-25 17:06:18 +0600

    Update Arch_install.sh

--- Notes/Arch_install.sh
+++ Notes/Arch_install.sh
@@ -4,11 +4,21 @@ iwctl #Wifi setting
     station wlan0123 get-networks #Lists all networks
     station wlan0123 connect "Network Name" #Connect to wifi
 fdisk
+    #to install UEFI, make a partition in fat-32 format, type, efi system, which is 200M in size
+    #mkfs.fat -F32 /dev/sda1
+
 mount /dev/sda1 /mnt
+    #if UEFI is used, then efi system partition should be mounted to /mnt/boot
+    #mount /dev/sda2 /mnt
+    #mkdir /mnt/boot
+    #mount /dev/sda1 /mnt/boot
+
+
+
 vim /etc/pacman.d/mirrorlist
     Server = http://mirror.xeonbd.com/archlinux/$repo/os/$arch
 
-pacstrap /mnt base base-devel linux linux-firmware linux-headers vim grub networkmanager network-manager-applet
+pacstrap /mnt base base-devel linux linux-firmware linux-headers efibootmgr vim grub networkmanager network-manager-applet
 genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt
 vim /etc/locale.gen
@@ -18,6 +28,11 @@ echo "Hostname" > /etc/hostname
 passwd
 systemctl enable NetworkManager
 grub-install /dev/sda #grub-install --force /dev/sda
+
+#if UEFI is used,
+#grub-install --efi-directory=/boot --bootloader-id=GRUB
+
+
 grub-mkconfig -o /boot/grub/grub.cfg
 exit
 umount -R /mnt
@@ -25,10 +40,17 @@ reboot
 nmtui #Setting Network
 useradd -m "username"
 usermod -G wheel "username"
+passwd username
+chfn -f "Full name" username
 vim /etc/sudoers
 su "username"
-pacman -S xorg lxsession "gdm or ly-git via yay" xfce4 xfce4-goodies pulseaudio pavucontrol xdg-user-dirs
-systemctl enable gdm
+pacman -S xorg lxsession "gdm or ly-git via yay" rofi xfce4 xfce4-goodies pulseaudio pavucontrol xdg-user-dirs xdg-utils sxhkd bspwm polybar sxhkd firefox gimp openshot nitrogen ibus ibus-m17n m17n-db ranger pcmanfm git moc xterm xfce4-terminal
+systemctl enable ly
+timedatectl set-ntp true
+timedatectl set-timezone Asia/Dhaka
+
+
+
 #create .xprofile file to auto-start program
 #sxhkd to hot keys
 #install nautilus for ftp
