83b7efc3bd3399cf4248eb01f9ed2ee41a3c0901
Raihan <114208911+draihanx@users.noreply.github.com>
2024-02-06 18:25:56 +0000

    Update setup.sh

--- hyprland/setup.sh
+++ hyprland/setup.sh
@@ -3,5 +3,5 @@ rm -rf $HOME/.config/kitty
 rm -rf $HOME/.config/hypr
 mkdir -p $HOME/.config
 mv config/* $HOME/.config/
-mv environment /etc/environment
+sudo mv environment /etc/environment
 #This script should be run as "sudo -su username"
